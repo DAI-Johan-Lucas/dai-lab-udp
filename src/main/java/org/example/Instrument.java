45432591f43e846bb3c3949e9485777c712f667a
LucasHSN <lucas.hussain@heig-vd.ch>
2024-01-18 12:01:00 +0100

    Remove comments

--- src/main/java/org/example/Instrument.java
+++ src/main/java/org/example/Instrument.java
@@ -9,12 +9,10 @@ public enum Instrument {
 
     private final String sound;
 
-    // Constructeur privé pour assigner la valeur à l'attribut sound
-    private Instrument(String sound) {
+    Instrument(String sound) {
         this.sound = sound;
     }
 
-    // Méthode pour obtenir le son de l'instrument
     public String sound() {
         return sound;
     }
