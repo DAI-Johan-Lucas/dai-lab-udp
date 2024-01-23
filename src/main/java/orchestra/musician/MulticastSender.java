8bbe971467259f49e8b1ae0e817d42bca7ac4518
LucasHSN <lucas.hussain@heig-vd.ch>
2024-01-23 16:08:49 +0100

    Fix creation of docker container

--- src/main/java/orchestra/musician/MulticastSender.java
+++ src/main/java/orchestra/musician/MulticastSender.java
@@ -21,8 +21,11 @@ class MulticastSender {
             System.exit(1);
         }
 
-        // Créer une instance de orchestra.Musician avec l'instrument spécifié
+        // Créer une instance de Musician avec l'instrument spécifié
         Musician musician = new Musician(args[0].toUpperCase());
+
+        System.out.println("Musician with a " + args[0] + " created");
+        System.out.println("Sounds sent :");
 
         // Créer un Timer pour planifier l'envoi périodique (toutes les secondes)
         Timer timer = new Timer();
@@ -39,7 +42,10 @@ class MulticastSender {
                     // Envoyer le message json à l'adresse IP de multicast et au port spécifié
                     var dest_address = new InetSocketAddress(IPADDR, PORT);
                     var packet = new DatagramPacket(musicianData, musicianData.length, dest_address);
+
                     socket.send(packet);
+                    System.out.print("*");
+
                 } catch (IOException ex) {
                     System.out.println(ex.getMessage());
                 }
